f4621c4fbf600fe3b3a9cb3c89ff4f604618162d
fridizkool <fsorgtaylor@gmail.com>
2022-09-18 13:26:03 -0400

    setup rust and added bevy

---
+++ game/target/debug/deps/game.d
@@ -0,0 +1,5 @@
+C:\Users\fsorg\Documents\cs1666\JerseyShoreTeam\game\target\debug\deps\game.exe: src\main.rs
+
+C:\Users\fsorg\Documents\cs1666\JerseyShoreTeam\game\target\debug\deps\game.d: src\main.rs
+
+src\main.rs:
---
+++ game/target/debug/game.d
@@ -0,0 +1,1 @@
+C:\Users\fsorg\Documents\cs1666\JerseyShoreTeam\game\target\debug\game.exe: C:\Users\fsorg\Documents\cs1666\JerseyShoreTeam\game\src\main.rs
