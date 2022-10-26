4acde4609740873753138bf6a574dd94cc950eed
Qiran Li <49622024+Qiran2022@users.noreply.github.com>
2022-10-25 23:30:02 -0400

    Add files via upload

--- game/src/parser.rs
+++ game/src/parser.rs
@@ -4,8 +4,22 @@ fn parser(input: &str) ->Vec<&str> {
     for s in split {
         strings.push(s);
     }  
-    strings
+    strings.clone()
+}
+fn stemmer(mut strings: Vec<&str>) ->Vec<&str>  {
+    let mut i=0;
+    let mut new_strings=Vec::new();
+    let stopword = vec!["a","about","above","across","after","afterwards","again","against","all", "almost","purpose"];
+    for s in strings{
+         if stopword.contains(&&s)==false{
+              new_strings.push(s);
+         }
+        i+=1;
+    }
+    new_strings
 }
 fn main(){
-    parser("This is a test purpose sentence");
+    let mut strings=parser("This is a test purpose sentence");
+    strings=stemmer(strings);
+    println!("{:?}", strings);
 }
