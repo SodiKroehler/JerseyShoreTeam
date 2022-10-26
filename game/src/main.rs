0cbac52db5798e8a64459cbeb6f99e01a7f1170c
Sodi Kroehler <sodikroehler@outlook.com>
2022-10-25 23:47:27 -0400

    added rover plugin

--- game/src/main.rs
+++ game/src/main.rs
@@ -6,6 +6,18 @@ use bevy::{
 #[derive(Component, Deref, DerefMut)]
 struct CreditTimer(Timer);
 
+<<<<<<< Updated upstream
+=======
+#[derive(Component)]
+struct Recycle{}
+static SCREEN_WIDTH:f32 = 1280.0;
+static SCREEN_HEIGHT:f32 = 720.0;
+
+//Rover stuff
+mod rover;
+use rover::RoverPlugin;
+
+>>>>>>> Stashed changes
 fn main() {
 	App::new()
 		.insert_resource(WindowDescriptor {
@@ -17,7 +29,16 @@ fn main() {
 		})
 		.add_plugins(DefaultPlugins)
 		.add_startup_system(setup)
+<<<<<<< Updated upstream
 		.add_system(roll_credits)
+=======
+		.add_event::<FolderSpawnEvent>()
+		//.add_system(roll_credits)
+		.add_system(move_everything)
+		.add_system(run_collisions)
+		.add_system(spawn_folder)
+		.add_plugin(RoverPlugin)
+>>>>>>> Stashed changes
 		.run();
 }
 
@@ -34,7 +55,173 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
 
 	info!("Hello world!");
 }
+<<<<<<< Updated upstream
 
+=======
+fn spawn_folder(
+	asset_server: Res<AssetServer>,
+	mut ev_spawnfolder : EventReader<FolderSpawnEvent>,
+	mut commands: Commands,
+	entity_cap : Query<&Folder>,
+){
+	let c = entity_cap.iter().count();
+	if c <= 4{
+		for ev in ev_spawnfolder.iter(){
+			commands.spawn()
+				.insert_bundle(SpriteBundle{
+				texture: asset_server.get_handle("folder.png"),
+				transform: Transform::from_translation(ev.0),
+				..default()
+				}).insert(Folder{
+				}).insert(Size{
+					size: Vec2{
+						x:37.0,
+						y:32.0,
+					}
+				})
+				.insert(Physics{
+					delta_x:0.0,
+					delta_y:0.0,
+					gravity:1.0,
+				});
+			//info!("spawned folder");
+		}
+	}
+}
+fn inbounds(trans : Vec3, size : Vec2,)->Vec3{
+	return Vec3{
+		x : trans.x.clamp(((-1.0*SCREEN_WIDTH)/2.0)+(size.x/2.0),((1.0*SCREEN_WIDTH)/2.0)-(size.x/2.0)),
+		y : trans.y.clamp(((-1.0*SCREEN_HEIGHT)/2.0)+(size.y/2.0),((1.0*SCREEN_HEIGHT)/2.0)-(size.y/2.0)),
+		z : 0.0
+	};
+}
+fn run_collisions(//first object is colliding into second
+	time: Res<Time>,
+	mut ev_spawnfolder : EventWriter<FolderSpawnEvent>,
+	mut obj_list: Query<(Entity, &Size, &mut Transform, &mut Physics, Option<&Player>, Option<&Recycle>)>,
+){
+	let mut obj_pairs = obj_list.iter_combinations_mut();
+	while let Some([(e1, object1, mut transform1, mut phys1, player1, recycle1), (e2, object2, mut transform2, mut phys2, player2, recycle2)]) = obj_pairs.fetch_next(){
+		if e1 != e2{//think this is pointless
+			let translation1 = &mut transform1.translation;
+			let translation2 = &mut transform2.translation;
+			let size1 = object1.size;
+			let size2 = object2.size;
+			const LAUNCH: f32 = 80.0;
+			const X_MAX_VEL: f32 = 100.0;
+			const Y_MAX_VEL: f32 = 100.0;
+			const RESTITUTION: f32 = 0.45;
+			const CUTOFF: f32 = 60.0;
+			let c = collide_circle::collide(*translation1,size1,*translation2,size2);
+			if c.is_some(){
+				//info!("collide");
+				if let Some(player1)=player1{
+					
+					if let Some(recycle2)=recycle2{	
+						ev_spawnfolder.send(FolderSpawnEvent(*translation2));
+					}
+				}
+				let temp1x = phys1.delta_x;
+				let temp1y = phys1.delta_y;
+				match c{
+					/*Some(Collision::Left)=>{phys1.delta_x=phys2.delta_x*RESTITUTION;phys2.delta_x=temp1x*RESTITUTION;},
+					Some(Collision::Right)=>{phys1.delta_x=phys2.delta_x*RESTITUTION;phys2.delta_x=temp1x*RESTITUTION;},
+					Some(Collision::Top)=>{phys1.delta_y=phys2.delta_y*RESTITUTION;phys2.delta_y=temp1y*RESTITUTION;},
+					Some(Collision::Bottom)=>{phys1.delta_y=phys2.delta_y*RESTITUTION;phys2.delta_y=temp1y*RESTITUTION;},
+					Some(Collision::Inside)=>{phys1.delta_x=phys2.delta_x*RESTITUTION;phys2.delta_x=temp1x*RESTITUTION;},
+					None=>(),*/
+					Some(Collision::Left)=>{phys2.delta_x+=LAUNCH;phys1.delta_x-=LAUNCH;},
+					Some(Collision::Right)=>{phys2.delta_x-=LAUNCH;phys1.delta_x+=LAUNCH;},
+					Some(Collision::Top)=>{phys2.delta_y-=LAUNCH;phys1.delta_y+=LAUNCH;},
+					Some(Collision::Bottom)=>{phys2.delta_y+=LAUNCH;phys1.delta_y-=LAUNCH;},
+					Some(Collision::Inside)=>{phys2.delta_x+=LAUNCH;phys1.delta_x-=LAUNCH;},
+					None=>(),
+				}
+				if phys1.delta_x.abs() < CUTOFF{
+					phys1.delta_x = 0.0;
+				}
+				if phys2.delta_x.abs() < CUTOFF{
+					phys2.delta_x = 0.0;
+				}
+				if phys1.delta_y.abs() < CUTOFF{
+					phys1.delta_y = 0.0;
+					phys1.gravity = 0.0;
+				}
+				if phys2.delta_y.abs() < CUTOFF{
+					phys2.delta_y = 0.0;
+					phys2.gravity = 0.0;
+				}
+				phys1.delta_y *= RESTITUTION;
+				phys2.delta_y *= RESTITUTION;
+				translation1.x += time.delta_seconds()*phys1.delta_x;
+				translation1.y += time.delta_seconds()*phys1.delta_y;
+				translation2.x += time.delta_seconds()*phys2.delta_x;
+				translation2.y += time.delta_seconds()*phys2.delta_y;
+				phys1.delta_x = phys1.delta_x.clamp(-X_MAX_VEL, X_MAX_VEL);
+				phys2.delta_x = phys2.delta_x.clamp(-X_MAX_VEL, X_MAX_VEL);
+				phys1.delta_y = phys1.delta_y.clamp(-Y_MAX_VEL, Y_MAX_VEL);
+				phys2.delta_y = phys2.delta_y.clamp(-Y_MAX_VEL, Y_MAX_VEL);
+				*translation1 = inbounds(*translation1, object1.size);
+				*translation2 = inbounds(*translation2, object2.size);
+			}
+			else{
+				//info!("no collide");
+				phys1.gravity = 1.0;
+				phys2.gravity = 1.0;
+			}
+		}
+	}
+}
+fn move_everything(
+	time: Res<Time>,
+	keyboard_input: Res<Input<KeyCode>>,
+	mut query: Query<(&mut Physics, &Size, &mut Transform, Option<&Player>)>,
+){
+	const X_ACCEL: f32 = 25.0;
+	const X_MAX_VEL: f32 = 100.0;
+	const GRAV: f32 = 10.0;
+	const Y_ACCEL: f32 = 200.0;
+	const FRICTION_SCALE: f32 = 0.75;
+	for (mut phys, object, mut transform, player) in query.iter_mut(){
+		let translation = &mut transform.translation;
+		//accelerate in horizontal
+		phys.delta_y -= GRAV * phys.gravity;
+		if let Some(player)=player{
+			//info!("y vel:{}",phys.delta_y);
+			let mut jumping = 0.0;
+			if keyboard_input.pressed(KeyCode::Left){
+				phys.delta_x-= X_ACCEL;
+				//info!("left");
+			}
+			if keyboard_input.pressed(KeyCode::Right){
+				phys.delta_x+= X_ACCEL;
+				//info!("right");
+			}
+			if keyboard_input.pressed(KeyCode::Space){
+				jumping = 1.0;
+				//info!("jump");
+			}
+			if translation.y <= -335.0 /*|| phys.delta_y.abs() < 1.0*/{//note: need to replace this with a function that checks for grounded for all physics entities
+				phys.delta_y = 0.0;
+				phys.delta_y += jumping * Y_ACCEL;
+			}
+		}
+		else{
+			if translation.y <= (-1.0*SCREEN_HEIGHT/2.0) +(object.size.y/2.0){
+				phys.delta_y = 0.0;
+			}
+		}
+		
+		
+		phys.delta_x = phys.delta_x.clamp(-X_MAX_VEL, X_MAX_VEL);
+		translation.x += time.delta_seconds()*phys.delta_x;
+		translation.y += time.delta_seconds()*phys.delta_y;
+		
+		phys.delta_x *= FRICTION_SCALE;
+		*translation = inbounds(*translation, object.size);
+	}
+}
+>>>>>>> Stashed changes
 fn roll_credits(
 	time: Res<Time>,
 	mut popup: Query<(&mut CreditTimer, &mut Transform)>
@@ -45,7 +232,7 @@ fn roll_credits(
 		if timer.just_finished() {
 			transform.translation.x -= 1280.;
             if counter == transform.translation.x {timer.pause()}
-			info!("Moved to next");
+			//info!("Moved to next");
 		}
 	}
 }
--- game/target/debug/game.d
+++
@@ -1,1 +0,0 @@
-C:\Users\fsorg\Documents\cs1666\JerseyShoreTeam\game\target\debug\game.exe: C:\Users\fsorg\Documents\cs1666\JerseyShoreTeam\game\src\main.rs
