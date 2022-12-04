e8d9713d9c8fc3797a4ea7be0e2c686364ddb199
Qiran Li <49622024+Qiran2022@users.noreply.github.com>
2022-12-04 10:13:56 -0500

    Update CONSTANTS.rs

--- src/CONSTANTS.rs
+++ src/CONSTANTS.rs
@@ -3,6 +3,6 @@ use bevy::prelude::*;
 pub const XP_BLUE: Color = Color::rgb(0.14, 0.364, 0.855);
 pub const START_GREEN: Color = Color::rgb(0.14, 0.364, 0.05);
 pub const PRESSED_BUTTON: Color = Color::rgb(0.35, 0.75, 0.35);
-pub const H: usize = 150;
+pub const H: usize = 300;
 pub const AMICABILITY: f64 = 100.0;
 pub const COS_DIST_THRESHOLD: f64 = 0.50;
